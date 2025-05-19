fe8f940a8f64bae7137b3ae1e0e0ebcde5ac4ef2
Alibiderci <urazshaevalibi@gmail.com>
2025-05-19 17:16:56 +0500

    Add fibonacci function

--- main.go
+++ main.go
@@ -10,8 +10,20 @@ func add(a, b int) int {
 	return a + b
 }
 
+func fibonacci(num int) int {
+	if num == 0 {
+		return 0
+	}
+	if num == 1 {
+		return 1
+	}
+
+	return fibonacci(num - 2) + fibonacci(num - 1)
+}
+
 func main() {
 	fmt.Println("Hello, Git!")
 	fmt.Println(greet("Alibi"))
 	fmt.Println("2 + 3 =", add(2,3))
+	fmt.Println("The 7th number in fibonacci sequence is", fibonacci(7))
 }

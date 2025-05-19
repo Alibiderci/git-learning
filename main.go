cc95ec80ab273c858f6926ca73f38ded2c11c00b
Alibiderci <urazshaevalibi@gmail.com>
2025-05-19 17:05:46 +0500

    Add calculation function

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ func greet(name string) string {
 	return fmt.Sprintf("Hello, %s!\n", name)
 }
 
+func add(a, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Println("Hello, Git!")
 	fmt.Println(greet("Alibi"))
+	fmt.Println("2 + 3 =", add(2,3))
 }

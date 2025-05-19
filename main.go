86fe73328c708aff709e6f186d54ce8069d4fb2b
Alibiderci <urazshaevalibi@gmail.com>
2025-05-19 16:12:49 +0500

    Add greeting function

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func greet(name string) string {
+	return fmt.Sprintf("Hello, %s!\n", name)
+}
+
 func main() {
 	fmt.Println("Hello, Git!")
+	fmt.Println(greet("Alibi"))
 }

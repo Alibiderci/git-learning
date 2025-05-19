650722a7c6f6842eed521400a7165a09d44c99b6
Alibiderci <urazshaevalibi@gmail.com>
2025-05-19 17:28:41 +0500

    Add factorial function

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ func greet(name string) string {
 	return fmt.Sprintf("Hello, %s!\n", name)
 }
 
+func factorial(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return n * factorial(n - 1)
+}
+
 func main() {
 	fmt.Println("Hello, Git!")
 	fmt.Println(greet("Alibi"))
+	fmt.Println("Factorial of 5 is", factorial(5))
 }

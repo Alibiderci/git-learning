cf8d514ffaeb21394283864b8d0b3203f85deb81
Alibiderci <urazshaevalibi@gmail.com>
2025-05-21 21:21:03 +0500

    Add reverse function

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import "fmt"
 
 func greet(name string) string {
-	return fmt.Sprintf("Hello, %s!")
+	return fmt.Sprintf("Hello, %s!", name)
 }
 
 func farewell(name string) string {
-	return fmt.Sprintf("Goodbye, %s!")
+	return fmt.Sprintf("Goodbye, %s!", name)
+}
+
+func reverse(word string) string {
+	if len(word) == 1 {
+		return word
+	}
+	return string(word[len(word)-1]) + reverse(word[:len(word)-1])
 }
 
 func main() {
 	fmt.Println("Hello, Git!")
 	fmt.Println(greet("Alibi"))
-	fmt.Println(farewell("Alibi"))
+	rev := reverse("alibI")
+	fmt.Println(farewell(rev))
 }

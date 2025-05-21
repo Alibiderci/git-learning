e763dae40afcb849bc9a7dfb60f6b0f9b4e5e129
Alibiderci <urazshaevalibi@gmail.com>
2025-05-21 19:57:42 +0500

    Add farewell function

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ func greet(name string) string {
 	return fmt.Sprintf("Hello, %s!")
 }
 
+func farewell(name string) string {
+	return fmt.Sprintf("Goodbye, %s!")
+}
+
 func main() {
 	fmt.Println("Hello, Git!")
 	fmt.Println(greet("Alibi"))
+	fmt.Println(farewell("Alibi"))
 }
